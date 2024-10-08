340849df4ac116dab45adea3904f786570a6530a
Sean Murphy <sean@iamseanmurphy.com>
2024-10-08 12:20:04 +0000

    1.4.4

--- wp-flickr-gallery/wp-flickr-gallery.php
+++ wp-flickr-gallery/wp-flickr-gallery.php
@@ -8,7 +8,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 Plugin Name: Album Photostream Profile For Flickr
 Plugin URI: https://awplife.com/
 Description: A Newly Amazing Different Most Powerful Responsive Easy To Use Flickr Plugin For WordPress
-Version: 1.4.3
+Version: 1.4.4
 Author: A WP Life
 Author URI: https://awplife.com/
 Text Domain: wp-flickr-gallery
